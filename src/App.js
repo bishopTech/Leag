6f0e3bc9bb99fa612d474fce951bcb084ba248c2
Chris Bishop <chris@bishop.tech>
2020-11-23 20:06:24 -0500

    added firebase and tested a basic save and get function to firestore

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useFirebase } from './components/Firebase/FirebaseContext'
+
+
+
+
 
 function App() {
   console.log(process.env.REACT_APP_API_KEY)
+  const firebase = useFirebase()
+  console.log(firebase)
+  const testData = {
+    name: "test",
+  }
+  const saveValue = () => {
+    firebase.db.collection('leagues').add(testData).then(documentReference => {
+      console.log("League Saved!", documentReference.id)
+    }).catch(error => {
+      console.log(error.message)
+    })
+  }
+
+  const getLeagues = () => {
+    console.log("Leagues: ")
+    firebase.db.collection('leagues').get()
+    .then(querySnapshot => {
+      const data = querySnapshot.docs.map( doc => doc.data())
+      console.log(data)
+    }).catch(err => {
+      console.log(err.message)
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +47,8 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={saveValue}>Save Value</button>
+        <button onClick={getLeagues}>Get Leagues</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Firebase from './components/Firebase/Firebase'
+import { FirebaseContext } from './components/Firebase/FirebaseContext'
+
 
 ReactDOM.render(
+  
   <React.StrictMode>
+    <FirebaseContext.Provider value={new Firebase()}>
     <App />
+    </FirebaseContext.Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
